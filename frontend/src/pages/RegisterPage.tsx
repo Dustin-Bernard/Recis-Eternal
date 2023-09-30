68fc9ff60b043c74416f3afd7c3478b3f5657097
vincentiusgian <gianjunius54@gmail.com>
2023-09-30 10:19:40 +0700

    add database

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,13 +1,32 @@
+import { useState, FormEvent } from "react";
 import { Container, Button } from "react-bootstrap";
 
 export default function RegisterPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function register(ev: FormEvent) {
+      ev.preventDefault();
+      const response = await fetch('http://localhost:4000/registerf', {
+        method: 'POST',
+        body: JSON.stringify({username,password}),
+        headers: {'Content-Type':'application/json'},
+      });
+      if (response.status === 200) {
+        alert('registration successful');
+      } else {
+        alert('registration failed');
+      }
+    }
+    
+
   return (
-    <Container className="container-margin">
+    <Container className="container-margin" onSubmit={register}>
       <div id="register-id">
         <h1>Register</h1>
         <form id="register-id" action="">
-          <input type="text" placeholder="username" />
-          <input type="password" placeholder="password" />
+          <input type="text" placeholder="username" value={username} onChange={ev => setUsername(ev.target.value)}/>
+          <input type="password" placeholder="password" value={password} onChange={ev => setPassword(ev.target.value)}/>
           <Button variant="primary" type="submit">
             Submit
           </Button>
