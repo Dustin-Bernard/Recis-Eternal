ef01397600508a15f9582ebbac1b1cef701020a5
vincentiusgian <gianjunius54@gmail.com>
2023-09-30 08:56:05 +0700

    footer to fixed bottom

--- frontend/src/components/FooterComponent.tsx
+++ frontend/src/components/FooterComponent.tsx
@@ -2,7 +2,7 @@ import { Container } from "react-bootstrap";
 
 const FooterComponent = () => {
   return (
-    <Container className="container-margin">
+    <Container className="container-margin fixed-bottom">
       <div>FooterComponent</div>
     </Container>
   );
