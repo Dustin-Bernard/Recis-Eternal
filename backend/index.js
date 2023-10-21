4ec2f4dbf783cd8733221c9ff0bf56883d20cbdb
vincentiusgian <gianjunius54@gmail.com>
2023-10-21 22:15:42 +0700

    backend 1

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const fs = require('fs');
 const salt = bcrypt.genSaltSync(10);
 const secret = 'ajkdflasjhfslah617t76dg78gd7812y';
 
-app.use(cors({ credentials: true, origin: 'http://localhost:5173', methods: ["GET", "POST"]}));
+app.use(cors({ credentials: true, origin: [''], methods: ["GET", "POST"]}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
